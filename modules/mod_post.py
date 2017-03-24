9dbcfc9309cf703e1866aa17bda886c8507df996
hongleifu <hongleifu@creditease.cn>
2017-03-24 14:37:32 +0800

    modify some ui

--- modules/mod_post.py
+++ modules/mod_post.py
@@ -123,6 +123,8 @@ def post_info(request):
         reply_user_list.append(user)
     community = db_model_community.select_by_id(community_id)
     best_reply = db_model_reply.select_best_by_post_id(post_id)
+    if best_reply.like_num < 3:
+      best_reply=None
     best_reply_user=None
     if best_reply != None:
       best_reply_user=db_model_user.select_by_id(reply.create_user_id)
