b12fde71d377ffc91eecd9b5faff84e787553117
liangxinxin <xinxinliang@example.com>
2017-04-18 16:01:16 +0800

    Merge branch 'master' into liangxinxin
    # Conflicts:
    #	modules/mod_post.py

--- modules/mod_post.py
+++ modules/mod_post.py
@@ -1,27 +1,28 @@
-#coding=utf-8
+# coding=utf-8
 import json
 import time
-import jieba
 
+import jieba
 from flask import session
 
 from db_interface import db_model_action
 from db_interface import db_model_action_type
 from db_interface import db_model_community
+from db_interface import db_model_inverted_index
 from db_interface import db_model_post
 from db_interface import db_model_reply
 from db_interface import db_model_reply_like_stat
 from db_interface import db_model_user
 from db_interface import db_model_user_community
-from db_interface import  db_model_inverted_index
-from modules import time_format
 from modules import mod_base64
 from modules import mod_lcs
+from modules import time_format
+
 default_page_no = 1
 default_num_perpage = 10
 default_community_id = 0
 default_post_id = 0
-default_precent =0.85
+default_precent = 0.85
 
 
 def service(request):
@@ -38,7 +39,6 @@ def service(request):
             return post_info(request)
         if communit_id != 0:
             return query_post_in_community(request)
-
 
 
 def find_match_post(request):
@@ -58,22 +58,22 @@ def find_match_post(request):
             lcs_title = []
             lcs_title = mod_lcs.printLcs(flag, title, len(title), len(post.title), lcs_title)
             lcs_title = ''.join(lcs_title)
-            #匹配百分比
+            # 匹配百分比
             percent = len(lcs_title.strip()) / float(len(title.strip()))
-            dict_post[id] = percent #dict:{'post_id':'percent',}
-            #print 'id', id, 'lcs_title', lcs_title, 'percent', '%.2f' % percent
-        #按percent 由大到小排序
+            dict_post[id] = percent  # dict:{'post_id':'percent',}
+            # print 'id', id, 'lcs_title', lcs_title, 'percent', '%.2f' % percent
+        # 按percent 由大到小排序
         dict_list = sorted(dict_post.items(), key=lambda e: e[1], reverse=True)
-        #从元组取值
+        # 从元组取值
         key = dict_list[0][0]
-        value =dict_list[0][1]
-        #保留2位小数
+        value = dict_list[0][1]
+        # 保留2位小数
         max_percent = round(value, 2)
         print 'key', key, 'value', value
-        ids=[]
+        ids = []
         if max_percent > default_precent:
-            for item in dict_list:#遍历元素是元组的集合
-                if item[1]>default_precent:
+            for item in dict_list:  # 遍历元素是元组的集合
+                if item[1] > default_precent:
                     ids.append(item[0])
                 else:
                     break
@@ -86,42 +86,36 @@ def find_match_post(request):
         return post_list
 
 
-
-
-
-
-
-
-
 def delete_post(request):
     param = json.loads(request.form.get('data'))
     post_id = param["post_id"]
     post = db_model_post.select_by_id(post_id)
     result = {}
-    if post==None:
-        result['code']=1
+    if post == None:
+        result['code'] = 1
         print 'delete fail,post is not find'
     try:
-        post.status=1
+        post.status = 1
         ISOTIMEFORMAT = '%Y-%m-%d %X'
         update_time = time.strftime(ISOTIMEFORMAT, time.localtime())
         post.last_update_time = update_time
         db_model_post.update(post)
         result['code'] = 0
-    except Exception,e:
-        result['code']=1
+    except Exception, e:
+        result['code'] = 1
         print '删除失败'
-    print 'delete post ',post_id
+    print 'delete post ', post_id
 
     return result
+
 
 def publish_post(request):
     print "now publish post request"
     # insert to db
     title = request.form.get("title")
     content = request.form.get("content")
-    path_type='post'
-    content = mod_base64.base64_hander(content,path_type)
+    path_type = 'post'
+    content = mod_base64.base64_hander(content, path_type)
     create_user_id = request.form.get("create_user_id", 0)
     login_user = db_model_user.select_by_id(create_user_id)
     community_id = request.form.get("community_id", 0)
@@ -131,25 +125,26 @@ def publish_post(request):
     create_time = time.strftime(ISOTIMEFORMAT, time.localtime())
     last_update_time = create_time
     print 'title:', title, 'content:', content, "user_id:", create_user_id, "community_id:", community_id
-    insert=db_model_post.insert(title, content, create_user_id, community_id, floor_num, create_time, last_update_time,status)
+    insert = db_model_post.insert(title, content, create_user_id, community_id, floor_num, create_time,
+                                  last_update_time, status)
     print "now insert to db"
 
     print "record action of create post"
-    action_content={}
-    action_content['post_id']=insert.id
-    db_model_action.insert(user_id=create_user_id,\
-           action_type_id=db_model_action_type.get_type_id('create_post'),\
-           action_detail_info=json.dumps(action_content, ensure_ascii = False),\
-           create_time=create_time)
+    action_content = {}
+    action_content['post_id'] = insert.id
+    db_model_action.insert(user_id=create_user_id, \
+                           action_type_id=db_model_action_type.get_type_id('create_post'), \
+                           action_detail_info=json.dumps(action_content, ensure_ascii=False), \
+                           create_time=create_time)
 
     login_user.post_num = login_user.post_num + 1
-    print "now update post_num to db",(login_user)
+    print "now update post_num to db", (login_user)
 
-    #insert into inverted_index
+    # insert into inverted_index
     last_update_time = create_time
     # fenci
     words = list(jieba.cut(title.strip(), cut_all=False))
-    print 'words length',len(words)
+    print 'words length', len(words)
     for word in words:
         inverted_index = db_model_inverted_index.select_by_word(word)
         if inverted_index != None:
@@ -227,19 +222,20 @@ def post_info(request):
     like_user_dict = {}
     for reply in reply_data.items:
         # add by lxx,like user start 2017-04-17
-        like_user_dict[reply.id]=select_like_user(reply.id)
+        like_user_dict[reply.id] = select_like_user(reply.id)
         # add by lxx,like user end
         user = db_model_user.select_by_id(reply.create_user_id)
         reply_user_list.append(user)
     community = db_model_community.select_by_id(community_id)
     best_reply = db_model_reply.select_best_by_post_id(post_id)
-    if best_reply!=None and best_reply.like_num < 3:
-      best_reply=None
-    best_reply_user=None
+    if best_reply != None and best_reply.like_num < 3:
+        best_reply = None
+    best_reply_user = None
     if best_reply != None:
-        best_reply_user=db_model_user.select_by_id(reply.create_user_id)
+        best_reply_user = db_model_user.select_by_id(reply.create_user_id)
         like_user_dict[reply.id] = select_like_user(reply.id)
     print "post data:", post_data, "reply data:", reply_data
+
     def get_reply_like_count(reply):
         reply_id = reply.id
         count = db_model_reply_like_stat.get_reply_like_count(reply_id)
@@ -254,13 +250,12 @@ def post_info(request):
         else:
             return (reply_id, False)
 
-
     liked_by_user = dict(map(is_reply_liked, reply_data.items))
     like_stats = dict(map(get_reply_like_count, reply_data.items))
 
     # return select value
-    return post_data, post_user, reply_data,like_user_dict, reply_user_list, community, page_no, len(\
-        reply_data.items), num_perpage, like_stats, liked_by_user,best_reply,best_reply_user
+    return post_data, post_user, reply_data, like_user_dict, reply_user_list, community, page_no, len( \
+        reply_data.items), num_perpage, like_stats, liked_by_user, best_reply, best_reply_user
 
 
 def select_good_post(request):
@@ -269,18 +264,19 @@ def select_good_post(request):
 
     # select db
     paginate = db_model_post.select_post_by_floor_num(page_no, num_perpage)
-    post_list_new=formate_post_time(paginate.items)
+    post_list_new = formate_post_time(paginate.items)
     print len(post_list_new)
-    return page_no,num_perpage,post_list_new
+    return page_no, num_perpage, post_list_new
 
 
 def formate_post_time(post_list):
-    post_list_new=[]
+    post_list_new = []
     for post_new in post_list:
         post_new.create_time = time_format.timestampFormat(post_new.create_time)
         post_list_new.append(post_new)
     print len(post_list_new)
     return post_list_new
+
 
 def select_goodpost_all():
     max_number = 1000;
@@ -290,11 +286,10 @@ def select_goodpost_all():
 
 
 def select_like_user(reply_id):
-    num_perpage =10
-    paginate = db_model_reply_like_stat.like_user(reply_id,default_page_no,num_perpage)
-    user_list =[]
+    num_perpage = 10
+    paginate = db_model_reply_like_stat.like_user(reply_id, default_page_no, num_perpage)
+    user_list = []
     for item in paginate.items:
         user = db_model_user.select_by_id(item.user_id)
         user_list.append(db_model_user.to_json(user))
     return user_list
-
